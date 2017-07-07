feb19967ff593f97f81763f4d0cc4ef7c58f6170
Brian Walker <techiecrow@gmail.com>
2017-07-07 04:02:38 +0100

    Almost got this working correctly, now I just need to add the broken block drops to the chests so fortune and silktouch also effects it.

--- src/main/java/com/techiecrow/OreRewards.java
+++ src/main/java/com/techiecrow/OreRewards.java
@@ -1,59 +1,21 @@
 package com.techiecrow;
 
 import com.techiecrow.listeners.OreCoal;
-import org.bukkit.ChatColor;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class OreRewards extends JavaPlugin
 {
 
+    // Enable the plugin
     public void onEnable()
     {
-        this.registerCommands();
+        // Register listeners
         this.registerListeners();
-        this.registerConfig();
     }
 
-    public void onDisable()
-    {
-
-    }
-
-    public boolean onCommand(CommandSender sender, Command command, String label, String[] args)
-    {
-        String prefix = ChatColor.translateAlternateColorCodes('&', this.getConfig().getString("Prefix"));
-        if (command.getName().equalsIgnoreCase("orerewards") && sender.hasPermission("orerewards.reload"))
-            if (args.length == 1)
-                if (args[0].equalsIgnoreCase("reload") && sender.hasPermission("orerewards.reload"))
-                {
-                    this.reloadConfig();
-                    this.getConfig();
-                    sender.sendMessage(prefix + ChatColor.GREEN + " Successfully reloaded the config!");
-                    return true;
-                } else
-                {
-                    sender.sendMessage(prefix + ChatColor.RED + " You need the \'orerewards.reload\' permission to use this command.");
-                    return false;
-                }
-        sender.sendMessage(prefix + ChatColor.RED + " Command not valid.");
-        return false;
-    }
-
-    private void registerCommands()
-    {
-
-    }
-
+    // Register listeners
     private void registerListeners()
     {
         getServer().getPluginManager().registerEvents(new OreCoal(this), this);
-    }
-
-    private void registerConfig()
-    {
-        this.getConfig().options().copyDefaults(true);
-        this.saveDefaultConfig();
     }
 }
--- src/main/java/com/techiecrow/listeners/OreCoal.java
+++ src/main/java/com/techiecrow/listeners/OreCoal.java
@@ -2,27 +2,25 @@ package com.techiecrow.listeners;
 
 import com.techiecrow.OreRewards;
 import org.bukkit.Material;
+import org.bukkit.block.Chest;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 
 import java.util.ArrayList;
-import java.util.Random;
 
 import static org.bukkit.GameMode.SURVIVAL;
 
 public class OreCoal implements Listener
 {
-    private final Random random = new Random();
     private OreRewards plugin;
+
+    //An array of blocks the player has placed
     private ArrayList<Object> PlayerPlaced = new ArrayList<>();
-
-    //int randomchance = this.plugin.getConfig().getInt("Iron Ore Reward Chance");
-
-    private ItemStack CoalOre1 = new ItemStack(Material.DIAMOND, 1);
 
     public OreCoal(OreRewards instance)
     {
@@ -31,35 +29,59 @@ public class OreCoal implements Listener
 
     }
 
-    private int getRandomNumber()
-    {
-        return random.nextInt(100) + 1;
-    }
-
-    private boolean RandomNumber()
-    {
-        return 50 >= getRandomNumber();
-    }
-
+    // Block break event
     @EventHandler(priority = EventPriority.MONITOR)
     public void BlockBreak(BlockBreakEvent e)
     {
-        if(e.getBlock().getType() == Material.COAL_ORE && RandomNumber() && e.getPlayer().getGameMode() == SURVIVAL)
+
+        // check if broken block is == coal ore and player is in survival
+        if (e.getBlock().getType() == Material.COAL_ORE && e.getPlayer().getGameMode() == SURVIVAL)
         {
-            if(PlayerPlaced.contains(e.getBlock().getLocation()))
+
+            //Check if player has placed block
+            if (PlayerPlaced.contains(e.getBlock().getLocation()))
             {
 
-            } else
+            }
+            // If block spawnned naturally
+            else
             {
-                e.getBlock().getWorld().dropItemNaturally(e.getBlock().getLocation(), CoalOre1);
+                // Cancel block break
+                e.setCancelled(true);
+
+                // Set block to chest
+                e.getBlock().setType(Material.CHEST);
+
+                // Get chest
+                Chest chest = (Chest) e.getBlock().getState();
+
+                // Get invenotry
+                Inventory inv = chest.getInventory();
+
+                // Define an item to put into chest
+                ItemStack ds = new ItemStack(Material.DIAMOND_SWORD, 1);
+
+                // Add item to chest
+                inv.setItem(26, ds);
+
+
+                // Start work on trying to get natrually dropped items into chest too
+//                Collection<ItemStack> drops = e.getBlock().getDrops();
+//
+//                for (ItemStack st : drops)
+//                {
+//                    inv = chest.getInventory();
+//                    inv.addItem(st);
+//                }
+//                e.getBlock().getDrops().clear();
             }
         }
     }
 
+    // Placed block event (used to check if player placed a block that we broke for the event above)
     @EventHandler(priority = EventPriority.MONITOR)
     public void BlockPlace(BlockPlaceEvent e)
     {
         PlayerPlaced.add(e.getBlockPlaced().getLocation());
     }
-
 }
